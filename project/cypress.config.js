3863a36ae1c55846a627abfa3e8768cf8beb482d
Andrew Chan <135365157+AndrewChan106@users.noreply.github.com>
2025-02-28 07:36:36 -0800

    Delete project directory

--- project/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    specPattern: "test/e2e/**/*.cy.js", // Point to new test location
-    baseUrl: "http://localhost:3000",  // Default server URL,
-    supportFile: false,
-  },
-});
