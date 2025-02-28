466a2430b96b3b9059289f09db288a9d31d26004
Andrew Chan <135365157+AndrewChan106@users.noreply.github.com>
2025-02-28 07:46:47 -0800

    Delete project directory

--- project/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const profileController = require('../controllers/profileController');
-
-router.get('/', profileController.displayPage);
-router.get('/setStore', profileController.setStore);
-
-module.exports = router;
