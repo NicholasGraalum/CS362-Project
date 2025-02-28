f0be3acf3ad16aca29684cf8d800edb3e91cf201
Andrew Chan <135365157+AndrewChan106@users.noreply.github.com>
2025-02-28 07:37:38 -0800

    Add files via upload

---
+++ project/test/integration/users.test.js
@@ -0,0 +1,12 @@
+// example test for example page
+const request = require('supertest');
+const { expect } = require('chai');
+const app = require('../../app');
+
+describe('Users Route', () => {
+  it('should return an HTML page with users', async () => {
+    const res = await request(app).get('/users');
+    expect(res.status).to.equal(200);
+    expect(res.text).to.include('All Users');
+  });
+});
