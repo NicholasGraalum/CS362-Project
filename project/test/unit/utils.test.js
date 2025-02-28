5a048e2d40731f5bf299e6138f8a8d6708fab502
Andrew Chan <135365157+AndrewChan106@users.noreply.github.com>
2025-02-28 07:11:02 -0800

    Delete project directory

--- project/test/unit/utils.test.js
+++
@@ -1,8 +0,0 @@
-// include tests for functions like meal macro calculation and list price estimate
-const { expect } = require('chai');
-
-describe('Basic Test', () => {
-  it('should add numbers correctly', () => {
-    expect(2 + 2).to.equal(4);
-  });
-});
