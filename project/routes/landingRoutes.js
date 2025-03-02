3612a064795ddb68a9accb8dca76ecb0af4a23c5
NicholasGraalum <nickgraalum@gmail.com>
2025-03-01 18:29:57 -0800

    Added landing page
    Added the landing page:
    -landingPage.handlebars
    -landingController.js
    -landingRoutes.js
    
    Set the default page in app.js to be the landing page

---
+++ project/routes/landingRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const landingController = require("../controllers/landingController")
+router.get('/', landingController.displayPage)
+
+module.exports = router;
