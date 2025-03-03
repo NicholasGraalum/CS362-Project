453d21e99a1510f48a6a7fa20b8106310c472298
Andrew Chan <135365157+AndrewChan106@users.noreply.github.com>
2025-03-02 17:27:34 -0800

    Add files via upload

---
+++ project/routes/mealsRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const mealsController = require("../controllers/mealsController");
+
+// Route to search meals by name or category
+router.get('/search', mealsController.searchMeals);
+
+// Route to create a new meal (POST request)
+router.post('/create', mealsController.createMeal);
+
+// Route to display all meals
+router.get('/', mealsController.getAllMeals);
+
+// Route to display a single meal page by ID
+router.get('/:id', mealsController.getSingleMeal);
+
+// Route to add a meal’s ingredients to the shopping list
+router.post('/add-ingredients', mealsController.addMealToList);
+
+module.exports = router;
