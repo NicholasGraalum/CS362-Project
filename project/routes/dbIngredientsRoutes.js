dbd1f1e7bd3d0f33eab6b2a7dd8b79476040f549
Andrew Chan <135365157+AndrewChan106@users.noreply.github.com>
2025-03-03 15:57:28 -0800

    Delete project directory

--- project/routes/dbIngredientsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const addIngredientsController = require("../controllers/dbIngredientsController");
-
-router.get('/:id', addIngredientsController.display);
-router.post('/add-to-meal', addIngredientsController.addIngredientToMeal);
-
-module.exports = router;
