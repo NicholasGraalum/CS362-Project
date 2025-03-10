8253c18540fda9af267baf746c641761e35a9fad
Kylan <Kyrileyj@gmail.com>
2025-03-09 23:38:45 -0700

    expand end to end test to cover new pages

--- project/test/e2e/web.cy.js
+++ project/test/e2e/web.cy.js
@@ -1,32 +1,52 @@
-// end to end system testing using cypress 
 describe("Meal Page", () => {
-    beforeEach(() => {
-      // Visit the meals page
-      cy.visit("http://localhost:3000/meals");
-    });
-  
-    it("should redirect to the correct meal detail page when clicking on a meal", () => {
-      // Assuming the meal data is available in the page and has a link with the structure `/meals/:id`
-      
-      // Find a meal link (assuming there's at least one meal on the page)
-      cy.get('.meal a').first().as('mealLink'); // Target the first meal's link
-  
-      // Get the meal ID from the link href attribute
-      cy.get('@mealLink')
-        .invoke('attr', 'href')  // Get the href attribute (should be something like "/meals/123")
-        .then((href) => {
-          // Verify the link is correct (i.e., it starts with /meals/)
-          expect(href).to.match(/^\/meals\/\d+/);
-          
-          // Click the meal link to navigate
-          cy.get('@mealLink').click();
-  
-          // Verify the URL is now the expected meal detail page URL
-          cy.url().should('include', href);  // It should match the href
-  
-          // Optionally, verify content on the meal detail page (e.g., check for meal name or id)
-          cy.contains('Ingredients');
-        });
-    });
+  beforeEach(() => {
+    // Visit the meals page
+    cy.visit("http://localhost:3000/meals");
   });
-  
+
+  it("should allow a user to log in and add a meal's ingredients to the list", () => {
+    // Click on the first meal to go to its detail page
+    cy.get('.meal a').first().as('mealLink');
+    cy.get('@mealLink')
+      .invoke('attr', 'href')
+      .then((href) => {
+        expect(href).to.match(/^\/meals\/\d+/);
+        cy.get('@mealLink').click();
+        cy.url().should('include', href);
+        
+        // Click on 'Add to List' button
+        cy.get('button.add-ingredients-button').click();
+        
+        // Should redirect to login page
+        cy.url().should('include', '/login');
+
+        // Enter login credentials
+        cy.get('#submit-email').type("alice@example.com");
+        cy.get('#submit-password').type("hashedpassword1");
+        
+        // Click login button
+        cy.get('#submit-login').click();
+
+        // Handle the alert by stubbing the alert and confirming it
+        cy.on('window:alert', (txt) => {
+          return true;
+        });
+
+        // After dismissing the alert, wait for the redirection by checking for an element on the meals page
+        cy.get('.meal').should('be.visible'); // Wait for a meal element to appear, ensuring the page has loaded
+        
+        // Now that the meals page is loaded, we can safely check the URL
+        cy.url().should('include', '/meals');
+
+        // Click on 'Add to List' button again for the first meal
+        cy.get('.meal .add-ingredients-button').first().click();
+
+        // Click on hamburger menu
+        cy.get('#hamburger-menu').click();
+
+        // Click on List button and verify redirection
+        cy.contains("List").click();
+        cy.url().should('include', '/list');
+      });
+  });
+});
